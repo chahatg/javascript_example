06d2d73cd7a85fb21e6a8d98fbe725efab908ed4
chahatg <44166542+chahatg@users.noreply.github.com>
2018-11-08 11:07:02 -0500

    final update

--- js/example.js
+++ js/example.js
@@ -1,26 +1,31 @@
 // ADD NEW ITEM TO END OF LIST
 var list =  document.getElementsByTagName("UL")[0];
-var li =  document.createElement("LI");
+
+var li =  document.createElement("li");
 li.appendChild(document.createTextNode("cream"));
 list.appendChild(li);
 
 
 // ADD NEW ITEM START OF LIST
-li = document.createElement("LI");
+li = document.createElement("li");
 li.appendChild(document.createTextNode("kale"));
 list.insertBefore(li, list.childNodes[0]);
 
 // ADD A CLASS OF COOL TO ALL LIST ITEMS
 var i;
+let List = document.querySelectorAll('li');
 
-for(i = 0; i < list.childNodes.length; i++){
-	document.getElementsByTagName("LI")[i].setAttribute("class", "cool");
+for(i = 0; i < List.length; i++){
+	List[i].className = 'cool';
 }
 
-// ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
-var itemsTotal = list.childNodes.length;
-var heading = document.getElementsByNameTag("H2")[0];
-li = document.createElement("span");
-span.appendChild(document.createTextNode(itemsTotal));
-heading.appendChild(span);
 
+
+// ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
+
+var itemsTotal = List.length;
+var heading = document.querySelector("h2");
+var el = document.createElement("span");
+var text = document.createTextNode(itemsTotal);
+el.appendChild(text);
+heading.appendChild(el);
